7a102b5c32f90d9ddf369a4ba9ff4a71ef01bdf8
Kinga <zawarczynska.ko@gmail.com>
2021-02-21 23:07:46 +0100

    fix(): style step button

--- src/components/StepButton.jsx
+++ src/components/StepButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../styles/StepButton.module.css';
 
 function StepButton({ title, onClick }) {
     return (
-        <div onClick={onClick}>
+        <div onClick={onClick} className={styles.stepButton}>
             {title}
         </div>
     )
